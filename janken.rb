0680934d0e241c9e9ca48c02a3a4e6ab43d03db1
Nekosuki523 <130686806+Nekosuki523@users.noreply.github.com>
2023-05-24 00:20:01 +0900

    janken.rb

--- janken.rb
+++ janken.rb
@@ -1,14 +1,13 @@
-require "csv"
-class JankenGame
 puts "あっち向いてホイゲームをします。"
 
 
-while true
+def jankenGame
     puts "じゃんけん..."
     puts "0(グー)１（チョキ）２（パー）３（たたかわない）"
+
 myHand = gets.chomp.to_i
 programHand = rand(3)
-case_pattern = ""
+
 
 if myHand == 3
     puts "また遊んでね！"
@@ -22,22 +21,27 @@ jankens = ["グー","チョキ","パー"]
 puts "ホイ！"
 puts "あなたは#{jankens[myHand]}を出しました。相手は#{jankens[programHand]}を出しました。"
 
-if  (myHand == 2 && programHand == 0 || myHand == 1 && programHand == 2 || myHand == 0 && programHand == 1)
-    puts "あなたの勝ちです！あっち向いて..."
-    win_lose = "win"
-    break
-    elsif (myHand == 0 && programHand == 2 || myHand == 2 && programHand == 1 || myHand == 1 && programHand == 0)
-    puts "残念...顔の方向を選んでね！"
-    win_lose = "lose"
-    break
-    else myHand == programHand 
+if  myHand == programHand
     puts "あいこです。もう一回！"
+    return true
+    
+    elsif (myHand == 2 && programHand == 0 || myHand == 1 && programHand == 2 || myHand == 0 && programHand == 1)
+    puts "じゃんけんに勝ちました！指の方向を選んでね！"
+    puts "------------------------"
+    $win_lose = "win"
+    acchimuite
+    
+    else 
+    puts "残念...顔の方向を選んでね！"
+    puts "------------------------"
+    $win_lose = "lose"
+    acchimuite
 end    
 end
 
-
 puts "------------------------"
 
+def acchimuite
 puts "顔の向きを選んでください。"
 puts "上(0) 下(1) 右(2) 左(3)"
 myFace = gets.to_i
@@ -47,23 +51,22 @@ face_direction = ["上","下","右","左"]
 
 puts "あなたの方向は#{face_direction[myFace]}。相手の方向は#{face_direction[programFace]}です。"
 
-        
-case win_lose
-when "win"
-    if myFace == programFace
-        puts "やったね！あなたの勝ちです！また遊んでね"
-        exit
+if $win_lose == "win" && myFace == programFace
+        puts "やったね！あなたの勝ちです！また遊んでね！"
+        return false
+    elsif $win_lose == "lose" && myFace == programFace
+        puts "残念...あなたの負けです...また遊んでね！"
+        return false
     else
-            puts "もう一回！じゃんけん..."
-            
+        puts "もう一回！じゃんけんのやり直し！"
+        puts "------------------------"
+        return true
     end
-    when "lose"
-    if myFace == programFace
-        puts "残念...あなたの負けです。また遊んでね"
-        exit
-    else
-            puts "もう一回！"
-    end
-end           
+end    
 
+
+next_game = true
+
+while next_game
+    next_game = jankenGame
 end
